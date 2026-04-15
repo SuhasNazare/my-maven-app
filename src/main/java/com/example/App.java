b2d8df04dc5ac539f8f99483a87a25a924ddfd8f
SUHAS N M <133192948+SuhasNazare@users.noreply.github.com>
2026-04-15 20:13:54 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,11 +3,18 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+        
         Thread.sleep(3000);
 
         driver.get("https://www.saucedemo.com/");
@@ -28,4 +35,3 @@ public class App {
         driver.quit();
     }
 }
-
